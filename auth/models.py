d7fb3b5dc0730db9f199a8cb83bc7b1ba3eab6af
gideon maina <gmainapro@yahoo.com>
2025-01-10 11:57:17 +0300

    update user model to table

--- auth/models.py
+++ auth/models.py
@@ -4,7 +4,7 @@ import sqlalchemy.dialects.postgresql as pg
 from pydantic import EmailStr
 
 
-class User(SQLModel):
+class User(SQLModel, table=True):
     __tablename__ = "users"
     uid: uuid.UUID = Field(
         sa_column=Column(pg.UUID, primary_key=True, nullable=False, default=uuid.uuid4)
