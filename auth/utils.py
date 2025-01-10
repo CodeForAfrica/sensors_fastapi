8ca9b72a6883f2876c13a87d586b757f2d3e503f
gideon maina <gmainapro@yahoo.com>
2025-01-10 15:41:37 +0300

    Use env variables for JWT

--- auth/utils.py
+++ auth/utils.py
@@ -1,9 +1,12 @@
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
-import jwt, uuid
+import jwt, uuid, os
 
 DEFAULT_TOKEN_EXPIRY = 60 * 60 * 24
-
+JWT_KEY = os.getenv("JWT_SECRET_KEY")  # ? use pydantic config instead
+JWT_ALGO = os.getenv("JWT_ALGORITHM")
+print("JWT_KEY", JWT_KEY)
+print("JWT_ALGORITHM", JWT_ALGO)
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
@@ -22,16 +25,17 @@ def create_access_token(data: dict, expiry: timedelta = None, refresh: bool = Fa
     )
     payload = {"user": data}
     payload["expires"] = expiry
+    payload["refresh"] = refresh
     payload["token_id"] = str(uuid.uuid4())
 
-    token = jwt.encode(payload=payload, key="myopenkey", algorithm="HS256")
+    token = jwt.encode(payload=payload, key=JWT_KEY, algorithm=JWT_ALGO)
 
     return token
 
 
 def decode_token(token: str) -> str:
     try:
-        token_data = jwt.decode(jwt=token, key="myopenkey", algorithms=["HS256"])
+        token_data = jwt.decode(jwt=token, key=JWT_KEY, algorithms=JWT_ALGO)
         return token_data
     except jwt.PyJWTError as error:
         print(error)
