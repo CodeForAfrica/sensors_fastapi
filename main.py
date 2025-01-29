3c56041d014709510e5ecbaa742abb7c49366cc5
gideon maina <gmainapro@yahoo.com>
2025-01-29 12:07:01 +0300

    Use lifespan context manager

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from auth.router import auth_router
 from db import init_postgres
 from sensors.router import sensors_router
+from contextlib import asynccontextmanager
 
 # sqlite_file_name = "sensorsafrica.db"
 # sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -10,13 +11,15 @@ from sensors.router import sensors_router
 # engine = create_engine(sqlite_url, connect_args=connect_args, echo=True)
 
 
-app = FastAPI()
-
-
-@app.on_event("startup")
-async def on_startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Initializing app ...")
     await init_postgres()
+    yield
+    print("Shutting down app ...")
 
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(auth_router)
 app.include_router(sensors_router)
